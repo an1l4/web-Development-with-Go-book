a88597191845fbfcd9587c73e30d4055330b19ab
Anila Soman <1995anila@gmail.com>
2022-12-15 13:37:35 +0530

    swap-upper-lower-

---
+++ swap-upper-lower/go.mod
@@ -0,0 +1,3 @@
+module github.com/an1l4/web-Development-with-Go-book
+
+go 1.19
---
+++ swap-upper-lower/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/an1l4/web-Development-with-Go-book/strcon"
+)
+
+func main() {
+	s := strcon.SwapCase("Gopher")
+	fmt.Println(s)
+}
